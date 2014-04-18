b1ad84a343b2dcab78d31346de1b088de27ca7a7
Alex Yakoubian <alex@alexyakoubian.com>
2014-04-18 00:52:05 -0700

    Prevent function name conflicts with other programs

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@
 
 ROOT=$(cd "$(dirname "$0")" && pwd)
 
-green () { printf "\033[32m$1\033[0m\n"; }
-yellow () { printf "\033[33m$1\033[0m\n"; }
-red () { printf "\033[31m$1\033[0m\n"; }
+_green () { printf "\033[32m$1\033[0m\n"; }
+_yellow () { printf "\033[33m$1\033[0m\n"; }
+_red () { printf "\033[31m$1\033[0m\n"; }
 
-link () {
+_link () {
     local from="$1" to="$2" from_="$ROOT/$1" to_="$HOME/$2"
-    yellow "Linking ~/$to => $from"
+    _yellow "Linking ~/$to => $from"
     if [ -d "$from_" ]; then
         ln -s "$from_/" "$to_"
     else
@@ -21,38 +21,38 @@ link () {
 
 # Install a file or directory to a given path by symlinking it, printing nice
 # things along the way.
-install () {
+_install () {
     local from="$1" to="$2" from_="$ROOT/$1" to_="$HOME/$2"
 
     if [ ! -e "$from_" ]; then
-        red "ERROR: $from doesn't exist! This is an error in $0"
+        _red "ERROR: $from doesn't exist! This is an error in $0"
         return 1
     fi
 
     if [ ! -e "$to_" ]; then
-        link "$from" "$to"
+        _link "$from" "$to"
     else
         local link
         link=$(readlink "$to_")
         if [ "$?" == 0 -a \( "$link" == "$from_" -o "$link" == "$from_/" \) ]; then
-            green "Link ~/$to => $from already exists!"
+            _green "Link ~/$to => $from already exists!"
         else
-            if ask "~/$to already exists. Would you like to replace it"; then
-                yellow "Moving ~/$to to ~/${to}.old"
+            if _ask "~/$to already exists. Would you like to replace it"; then
+                _yellow "Moving ~/$to to ~/${to}.old"
                 mv "$to_" "${to_}.old"
-                link "$from" "$to"
+                _link "$from" "$to"
             else
-                red "Error linking ~/$to to $from: $to already exists!"
+                _red "Error linking ~/$to to $from: $to already exists!"
             fi
         fi
     fi
 }
 
-install_dot () {
-    install "$1" ".$1"
+_install_dot () {
+    _install "$1" ".$1"
 }
 
-ask () {
+_ask () {
     local question="$1" default_y="$2" yn
     if [ -z "$default_y" ]; then
         read -p "$question [y/N]? "
@@ -67,31 +67,31 @@ ask () {
     fi
 }
 
-install_dot "bash_profile"
-install_dot "bashrc"
-install_dot "vimrc"
-install_dot "vim"
-install_dot "gitconfig"
-install_dot "gitignore"
-install_dot "tmux.conf"
-install_dot "gemrc"
+_install_dot "bash_profile"
+_install_dot "bashrc"
+_install_dot "vimrc"
+_install_dot "vim"
+_install_dot "gitconfig"
+_install_dot "gitignore"
+_install_dot "tmux.conf"
+_install_dot "gemrc"
 
 if ! git config --get-regexp submodule* > /dev/null; then
-    if ask "Initialize submodules" "Y"; then
+    if _ask "Initialize submodules" "Y"; then
         git submodule init
         git submodule update
     fi
 fi
 
 if command -v vim > /dev/null ; then
-    if ask "Install Vundle for Vim" "Y"; then
+    if _ask "Install Vundle for Vim" "Y"; then
         vim +BundleInstall +qall
     fi
 else
-    red "Error installing Vundle: Vim is not installed!"
+    _red "Error installing Vundle: Vim is not installed!"
 fi
 
-green "All done! You might still have to do some stuff for everything to work properly:
+_green "All done! You might still have to do some stuff for everything to work properly:
 
     - Install a Powerline-patched font on your system and set it as the font
       on your terminal emulator. This is needed to work with vim-airline.
