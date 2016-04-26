347900529c153e95f43d462ab51c94499c29046b
Alex Yakoubian <alex@alexyakoubian.com>
2016-04-26 16:32:33 -0700

    Move Homebrew bash completion loader to separate Homebrew config

--- profile.d/10-completion.sh
+++ profile.d/10-completion.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Homebrew bash-completion
-if command -v brew > /dev/null && [ -f "$(brew --prefix)/etc/bash_completion" ]; then
-    source "$(brew --prefix)/etc/bash_completion"
-fi
-
 # System-wide bash-completion
 for completion in "/etc/bash_completion" "/usr/local/etc/bash_completion"; do
     if [ -f "$completion" ] && ! shopt -oq posix; then
--- profile.d/30-homebrew.sh
+++ profile.d/30-homebrew.sh
@@ -2,7 +2,14 @@
 
 path_unshift "$HOME/.homebrew/bin"
 
-# Opt-out of Homebrew's analytics
 if command -v brew > /dev/null ; then
+
+    # Opt-out of analytics
     export HOMEBREW_NO_ANALYTICS=1
+
+    # Load bash completion
+    if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
+        source "$(brew --prefix)/etc/bash_completion"
+    fi
+
 fi
